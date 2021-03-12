d5470b98fa1594db6c82175da0c2c0d6f2e4c954
ely966 <46519177+ely966@users.noreply.github.com>
2021-03-12 22:56:57 +0100

    Avance

---
+++ Logica/Constantes.java
@@ -0,0 +1,38 @@
+package Logica;
+
+public class Constantes {
+	protected final static int ALTO =10;
+	protected final static int ANCHO = 10;
+	protected final static int NUM_ARBOLES = 7;
+	protected final static int NUM_ROCAS = 5;
+	protected final static int NUM_GEMAS = 5;
+	protected final static int NUM_POCIONES = 3;
+	protected final static int NUM_DINERO = 2;
+	protected static char ROCA = 'R';
+	protected static char ARBOL = 'A';
+	protected static char GEMA = 'G';
+	protected static char POCION = 'P';
+	protected static char DINERO = 'D';
+	protected static char NOMBRE_JUGADORES[]= {'X','Y','Z','U','V','W'};
+	protected static int ELFO_VELOCIDAD =7;
+	protected static int ELFO_FUERZA= 5;
+	protected static int ELFO_MAGIA = 6;
+	protected static int OGRO_VELOCIDAD =4;
+	protected static int OGRO_MAGIA =4;
+	protected static int OGRO_FUERZA=7;
+	protected static int GUERRERO_VELOCIDAD=5;
+	protected static int GUERRERO_MAGIA=5;
+	protected static int GUERRERO_FUERZA=6;
+	protected static int MAGO_VELOCIDAD =6;
+	protected static int MAGO_MAGIA =7;
+	protected static int MAGO_FUERZA=4;
+	protected static int MAGIA_ARBOL =7;
+	
+	
+	public Constantes() {
+		super();
+	}
+	
+	
+	
+}
---
+++ Logica/Juego.java
@@ -0,0 +1,39 @@
+package Logica;
+
+import Elementos.Elemento;
+import Elementos.Jugador;
+
+public class Juego {
+	
+	protected Jugador jugadores [];
+	protected Elemento tablero [][];
+	protected int alto;
+	protected int ancho;
+	protected int  numJugadores ;
+	protected boolean finished =false;
+	protected int jugadoJuega ;
+	
+	
+	public void isTerminado() {
+		
+	}
+	
+	public String imprimeNombreJugadores() {
+		
+		jugadores[].getCaracter();
+	}
+	
+	public String imprimeValoresJugadores() {
+		String valores =jugadores.toString(); 
+		return valores;
+	}
+	
+	
+	
+public void proximoJugador() {
+		
+	}
+	
+	
+	
+}
